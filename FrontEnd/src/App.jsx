fd5ce418557dbe7eb4f67852ac710473101c940b
Kushan Ruwanpathirana <kushanruwan1@gmail.com>
2025-07-23 18:11:52 +0530

    update nav bar

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div className=''>
-      <Navbar/>
     </div>
   )
 }
