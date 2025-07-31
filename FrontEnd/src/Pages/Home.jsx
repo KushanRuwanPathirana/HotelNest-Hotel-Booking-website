bf7f80b1df301021731b7c8577ebfeb3e7d9fd65
Kushan Ruwanpathirana <kushanruwan1@gmail.com>
2025-07-31 17:49:03 +0530

    Add Exclusiveoffers Section

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import FeaturedDestinations from '../Components/FeaturedDestinations'
+import Exclusiveoffers from '../Components/Exclusiveoffers'
 
 const Home = () => {
   return (
     <>
         <Hero/>
         <FeaturedDestinations/>
+        <Exclusiveoffers/>
     </>
   )
 }
