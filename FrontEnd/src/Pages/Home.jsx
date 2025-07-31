3f08823836969176fda7ca53f6e185da50bb644e
Kushan Ruwanpathirana <kushanruwan1@gmail.com>
2025-08-01 01:03:21 +0530

    completed Home page
    News Letter component & Footer

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero'
 import FeaturedDestinations from '../Components/FeaturedDestinations'
 import Exclusiveoffers from '../Components/Exclusiveoffers'
 import Testermonial from '../Components/Testermonial'
+import Newsletter from '../Components/Newsletter'
 
 const Home = () => {
   return (
@@ -11,8 +12,10 @@ const Home = () => {
         <FeaturedDestinations/>
         <Exclusiveoffers/>
         <Testermonial/>
+        <Newsletter/>
+        
     </>
   )
 }
 
-export default Home
+export default Home 
