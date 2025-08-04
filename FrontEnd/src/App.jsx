2501b382a03fafca288c2164ebc1c4c6dd17c1db
Kushan Ruwanpathirana <kushanruwan1@gmail.com>
2025-08-05 02:11:18 +0530

    Create Hotel Rooms page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './Pages/Home';
+import Footer from './Components/Footer';
+import AllRooms from './Pages/AllRooms';
 
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
-    <div className=''>
+    <div>
       {!isOwnerPath && <Navbar/>}
       <div className='min-h-[70vh]'>
           <Routes>
               <Route path='/' element={<Home/>} />
+              <Route path='/rooms' element={<AllRooms/>} />
           </Routes>
       </div>
     </div>
